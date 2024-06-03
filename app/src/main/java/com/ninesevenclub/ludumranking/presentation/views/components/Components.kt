011266856e95a3225f5fc825b857a4f19f7292a8
ckoffee <carmonajimeneznicolas@gmail.com>
2024-06-03 19:08:22 +0200

    Composable text

--- app/src/main/java/com/ninesevenclub/ludumranking/presentation/views/components/Components.kt
+++ app/src/main/java/com/ninesevenclub/ludumranking/presentation/views/components/Components.kt
@@ -1,2 +1,9 @@
 package com.ninesevenclub.ludumranking.presentation.views.components
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MyText(){
+    Text(text = "Hellooooooooo!!")
+}
