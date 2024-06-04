403eba77fee84c4cdbb4ffa57c12b57d7c6ae6aa
IvanTJ97 <ivan.tejfar@gmail.com>
2024-06-04 20:38:19 +0200

    + Aun + Cositas

--- app/src/main/java/com/ninesevenclub/ludumranking/data/remote/LRService.kt
+++ app/src/main/java/com/ninesevenclub/ludumranking/data/remote/LRService.kt
@@ -7,12 +7,13 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 
 interface LRService {
     //https://rawg.io/apidocs 192d53844b6b46d782e8cc8728ae44ff
     @GET("games?key={api_key}")
-    fun listGames(@Path("api_key") apiKey: String = "192d53844b6b46d782e8cc8728ae44ff"): GameResponse
+    fun listGames(@Query("api_key") apiKey: String = "192d53844b6b46d782e8cc8728ae44ff"): GameResponse
 
     companion object {
         private const val BASE_URL = "https://api.rawg.io/api/"
