8974039705cf92e9790906d9ce5e23c362883744
Hector <hectorplausmuste@gmail.com>
2024-06-04 20:23:15 +0200

    DetailView Strucutre

--- app/src/main/java/com/ninesevenclub/ludumranking/presentation/views/DetailView.kt
+++ app/src/main/java/com/ninesevenclub/ludumranking/presentation/views/DetailView.kt
@@ -1,6 +1,5 @@
 package com.ninesevenclub.ludumranking.presentation.views
 
-import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.compose.foundation.Image
@@ -11,11 +10,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
-import androidx.compose.material.icons.rounded.ShoppingCart
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -23,10 +19,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.core.app.Person
-import androidx.lifecycle.ViewModel
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.ninesevenclub.ludumranking.R
 import com.ninesevenclub.ludumranking.presentation.viewmodel.LRViewModel
