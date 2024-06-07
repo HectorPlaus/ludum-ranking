20fc526bfa5977da9d9f5e2781f5d98a23bfe587
LeoMe <eleomerelo@gmail.com>
2024-06-07 16:34:32 +0200

    back arrow on SearchView.kt

--- app/src/main/java/com/ninesevenclub/ludumranking/presentation/views/SearchView.kt
+++ app/src/main/java/com/ninesevenclub/ludumranking/presentation/views/SearchView.kt
@@ -4,17 +4,21 @@ import android.widget.SearchView
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
@@ -23,8 +27,10 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.ninesevenclub.ludumranking.presentation.viewmodel.LRViewModel
@@ -47,36 +53,50 @@ fun SearchView(viewModel: LRViewModel, navController: NavHostController) {
                 .fillMaxSize()
         ) {
             // Search Bar using TextField
-            TextField(
-                value = searchText,
-                onValueChange = { viewModel.onSearchTextChange(it) },
-                label = {
-                    if (searchText.isEmpty()) {
-                        Text("Search")
-                    }
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-                leadingIcon = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                IconButton(onClick = { navController.navigateUp() }) {
                     Icon(
-                        imageVector = Icons.Default.Search,
-                        contentDescription = "Search"
+                        Icons.Rounded.ArrowBack,
+                        contentDescription = "BACK",
+                        tint = Color.LightGray,
+                        modifier = Modifier.size(30.dp)
                     )
-                },
-                trailingIcon = {
-                    if (searchText.isNotEmpty()) {
-                        IconButton(onClick = { viewModel.onSearchTextChange("") }) {
-                            Icon(
-                                imageVector = Icons.Default.Close,
-                                contentDescription = "Clear Search"
-                            )
+                }
+
+                TextField(
+                    value = searchText,
+                    onValueChange = { viewModel.onSearchTextChange(it) },
+                    label = {
+                        if (searchText.isEmpty()) {
+                            Text("Search")
                         }
-                    }
-                },
-                singleLine = true,
-                colors = TextFieldDefaults.textFieldColors(focusedLabelColor = Color.Transparent)
-            )
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(24.dp, end = 64.dp),
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Search"
+                        )
+                    },
+                    trailingIcon = {
+                        if (searchText.isNotEmpty()) {
+                            IconButton(onClick = { viewModel.onSearchTextChange("") }) {
+                                Icon(
+                                    imageVector = Icons.Default.Close,
+                                    contentDescription = "Clear Search"
+                                )
+                            }
+                        }
+                    },
+                    singleLine = true,
+                    colors = TextFieldDefaults.textFieldColors(focusedLabelColor = Color.Transparent),
+                    textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Start)
+
+                )
+            }
+
 
             // Display Search Results
             LazyColumn {
@@ -84,11 +104,12 @@ fun SearchView(viewModel: LRViewModel, navController: NavHostController) {
                     Text(
                         text = game.name,
                         modifier = Modifier
-                            .padding(8.dp)
+                            .padding(16.dp)
                             .clickable
                             {
                                 viewModel.getGameDetails(game.id)
-                                navController.navigate(Routes.DetailView.createRoute(game.id)) }
+                                navController.navigate(Routes.DetailView.createRoute(game.id))
+                            }
                     )
                 }
             }
